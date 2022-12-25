5b7d11ade0d24b2b58d6de639371ca74ad5fa004
buyrak <siyahmerkez@gmail.com>
2022-12-25 23:23:09 +0100

    image generation is added.

---
+++ main.py
@@ -0,0 +1,34 @@
+import openai as openai
+import requests
+import shutil
+from PIL import Image
+
+openai.api_key = "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"
+
+
+def createImageWithText(keyword, search_key):
+    for i in range(0, 100):
+        response = openai.Image.create(
+            prompt=keyword,
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        print(image_url)
+
+        file_name = f"{search_key}-{i}.png"
+
+        res = requests.get(image_url, stream=True)
+
+        if res.status_code == 200:
+            with open(f"src/{file_name}", 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ', file_name)
+        else:
+            print('Image Couldn\'t be retrieved')
+
+
+if __name__ == '__main__':
+    keyword = input("Please enter a few words: ")
+    search_key = input("Please enter your keyword(to save): ")
+    createImageWithText(keyword, search_key)
